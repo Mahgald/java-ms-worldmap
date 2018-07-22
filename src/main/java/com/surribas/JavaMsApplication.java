9f99f8d3c0e691e89365931602840ce31c244379
Unknown <ricardo.surribas@gmail.com>
2018-07-22 18:07:11 -0300

    Version 0.0.1
    - First Commit

---
+++ src/main/java/com/surribas/JavaMsApplication.java
@@ -0,0 +1,12 @@
+package com.surribas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMsApplication.class, args);
+	}
+}
