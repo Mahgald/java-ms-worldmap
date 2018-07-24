5df4ca51ec846e98ff533fb23f4649196fae7316
Unknown <ricardo.surribas@gmail.com>
2018-07-24 17:13:19 -0300

    Upload 0.0.3
    - Eureka Discover

--- src/main/java/com/surribas/JavaMsApplication.java
+++ src/main/java/com/surribas/JavaMsApplication.java
@@ -2,8 +2,10 @@ package com.surribas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JavaMsApplication {
 
 	public static void main(String[] args) {
